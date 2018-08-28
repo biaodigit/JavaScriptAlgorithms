943d8c5cc6fdf491593cbf99de47b0eb2a5e330d
luowendi <15622285015@163.com>
2018-08-28 18:18:49 +0800

    commit

--- Sort/SortTestHelper.js
+++ Sort/SortTestHelper.js
@@ -1,16 +1,16 @@
 /**
  * Created by di on 2017/6/10.
  */
-var wrap = function (arr,curr,min) {
+var wrap = function (arr, curr, min) {
     var temp = arr[curr];
-    arr[curr]= arr[min];
+    arr[curr] = arr[min];
     arr[min] = temp;
 }
 
-var generateRandomArray = function (n,rangeL,rangeR) {
+var generateRandomArray = function (n, rangeL, rangeR) {
     var arr = [];
-    for(var i=0;i<n;i++){
-        arr.push(Math.floor(Math.random()*(rangeR-rangeL+1)))
+    for (var i = 0; i < n; i++) {
+        arr.push(Math.floor(Math.random() * (rangeR - rangeL + 1)))
     }
     return arr;
 }
@@ -18,8 +18,8 @@ var generateRandomArray = function (n,rangeL,rangeR) {
 var testSort = function (fn) {
     var start = new Date();
     console.log(start)
-    var arr = [].slice.call(arguments,1);
-    fn.apply(this,arr);
+    var arr = [].slice.call(arguments, 1);
+    fn.apply(this, arr);
     var end = new Date();
     console.log(end);
     var time = end - start;
@@ -28,8 +28,8 @@ var testSort = function (fn) {
 
 var isSorted = function (n) {
     var arr = [];
-    for(var i=0;i<n;i++){
-       arr.push(i);
+    for (var i = 0; i < n; i++) {
+        arr.push(i);
     }
     return arr;
 }
